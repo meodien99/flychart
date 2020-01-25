2045d1be11de6c985b18f1756a63b3a8df6947f1
Nam HD <namhd@mtbit.com>
2020-01-25 16:02:06 +0700

    completed priceScale

---
+++ src/models/LayoutOptions.ts
@@ -0,0 +1,7 @@
+export type LayoutOptions = {
+    lineColor: string,
+    backgroundColor: string,
+    textColor: string,
+    fontSize: number,
+    fontFamily: string
+};
--- src/models/times.ts
+++ src/models/times.ts
@@ -1,5 +1,7 @@
 import { Nominal } from "../helpers/nominal";
 import { Coordinate } from "./Coordinate";
+import { BarsRange } from "./BarsRange";
+import { upperbound, lowerbound } from "../helpers/binary-search";
 
 export type UTCTimestamp = Nominal<number, 'UTCTimestamp'>;
 
@@ -25,4 +27,25 @@ export type TickMark = {
 export type TimedValue = {
     time: TimePointIndex,
     x: Coordinate
+}
+
+
+export type SeriesItemsIndexesRange = {
+    from: number,
+    to: number
+};
+
+function lowerBoundItemsCompare(item: TimedValue, time: TimePointIndex): boolean {
+    return item.time < time;
+}
+
+function upperBoundItemsCompare(time: TimePointIndex, item: TimedValue): boolean {
+    return time < item.time;
+}
+
+export function visibleTimedValue(items: TimedValue[], range: BarsRange): SeriesItemsIndexesRange {
+    const from = lowerbound<TimedValue, TimePointIndex>(items, range.firstBar(), lowerBoundItemsCompare);
+    const to = upperbound<TimedValue, TimePointIndex>(items, range.lastBar(), upperBoundItemsCompare);
+
+    return {from, to};
 }
